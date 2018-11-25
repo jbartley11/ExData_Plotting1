eb38b046c64f5de0698af61c41eab118765abcf2
Jason Bartley <jbartley11@gmail.com>
2018-11-25 07:12:40 -0600

    modified plot1.R to use colClasses when reading

--- plot1.R
+++ plot1.R
@@ -3,19 +3,23 @@
 # set working directory
 setwd("/Users/jasonbartley/Development/courses/ExData_Plotting1")
 
-# load data
+# read data
+classes <- c("character", "character", "numeric", "numeric",
+             "numeric", "numeric", "numeric", "numeric",
+             "numeric")
+
 data <- read.table("./data/household_power_consumption.txt",
                    sep=";",
-                   header=TRUE)
+                   header=TRUE,
+                   stringsAsFactors=FALSE,
+                   colClasses=classes,
+                   na.strings=c("?"))
 
 # subset for 2007-02-01 and 2007-02-02
 data <- subset(data, data$Date %in% c("2/1/2007", "2/2/2007"))
 
-# convert global active power to numeric
-data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
-
 # plot histogram
-hist(data2$Global_active_power,
+hist(data$Global_active_power,
      col="red",
      main="Global Active Power",
      xlab="Global Active Power (kilowatts)")
--- plot2.R
+++ plot2.R
@@ -1,21 +1,31 @@
-# produce histogram plot of active power and frequency
+# produce histogram plot of global active power over time
 
 # set working directory
 setwd("/Users/jasonbartley/Development/courses/ExData_Plotting1")
 
 # load data
+classes <- c("character", "character", "numeric", "numeric",
+             "numeric", "numeric", "numeric", "numeric",
+             "numeric")
+
 data <- read.table("./data/household_power_consumption.txt",
                    sep=";",
-                   header=TRUE)
+                   header=TRUE,
+                   stringsAsFactors=FALSE,
+                   colClasses=classes,
+                   na.strings=c("?"))
 
 # subset for 2007-02-01 and 2007-02-02
 data <- subset(data, data$Date %in% c("2/1/2007", "2/2/2007"))
-
+str(data)
 # make copy for testing
 data2 <- data
 
 # convert global active power to numeric
 data2$Global_active_power <- as.numeric(as.character(data2$Global_active_power))
+
+# make datetime field
+
 
 head(data2)
 summary(data)
